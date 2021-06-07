f3209597e24b9cf06aa728a3a8aa39aeb7b0d420
Josh Long <josh@joshlong.com>
2021-06-07 04:38:20 -0700

    run

--- gradle-build/run.sh
+++ gradle-build/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cd $(dirname .)
+cd $(dirname $0) 
+pwd
 cd build/native-image
 ./native-tests # this is a standard name for the native binary containing the test code
 ./gradle-build # this is named for the artifactId of the module
