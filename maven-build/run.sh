d442dc99faaa97752247f8a41cd8e23b31046789
Josh Long <josh@joshlong.com>
2021-06-07 04:43:17 -0700

    maven

--- maven-build/run.sh
+++ maven-build/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $(dirname .)
+cd $(dirname $0)
 cd target
 ./native-tests # this is a standard name for the native binary containing the test code
 ./maven-build # this is named for the artifactId of the module
