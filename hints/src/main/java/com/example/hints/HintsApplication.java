426535aa9e2b3a45ebab08d9ad5f5df535eba832
Josh Long <josh@joshlong.com>
2021-06-07 22:50:43 -0700

    hints

--- hints/src/main/java/com/example/hints/HintsApplication.java
+++ hints/src/main/java/com/example/hints/HintsApplication.java
@@ -1,12 +1,35 @@
 package com.example.hints;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.SerializationHint;
 
-@ResourceHint(patterns = "banner.txt") // 1.
+import java.io.*;
+
+
+/*
+	* Resources:
+	* I couldn't figure out how to need this hint in the first place, at first!
+	* the library on which this code depends, and in which this custom resource lives,
+	* concatenates two strings with data in a folder to finally break so that we can use this!
+	*/
+@ResourceHint(patterns = "data/airline-safety.csv")
+
+/*
+	* Serialization:
+	* this demonstrates how to contribute a serialization hint on the odd occasion you want
+	* to, em, serialize a Java object. Hey, stranger things have happened! Quartz requires this!
+	*/
+@SerializationHint (types = Customer.class)
+
 @SpringBootApplication
 public class HintsApplication {
 
@@ -18,16 +41,37 @@ public class HintsApplication {
 	ApplicationRunner runner() {
 		return event -> System.out.println("hello, world!");
 	}
+
+	@Bean
+	ApplicationRunner serialization(
+		@Value("file:///${user.home}/output") Resource resource) {
+		return args -> {
+			var written = resource.getFile();
+			var customer = new Customer(1, "Andy");
+
+			try (var out = new ObjectOutputStream(new FileOutputStream(written))) {
+				out.writeObject(customer);
+			}
+
+			try (var in = new ObjectInputStream( new FileInputStream( written))) {
+				var readCustomer = (Customer) in.readObject() ;
+				System.out.println("read: " + readCustomer);
+			}
+		};
+	}
+
+
 }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer implements Serializable {
 
 
-// 1. Resources
-//
-//
+	private int id;
+	private String name;
 
-// 2. reflection
-//
-//
+}
 
 // need to demonstrate serialization
 // need to demonstrate jdk proxies
---
+++ library-with-resource/src/main/java/bootiful/BootifulCsvAnalyser.java
@@ -0,0 +1,29 @@
+package bootiful;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.InputStreamReader;
+
+@Configuration
+class BootifulCsvAnalyser {
+
+	@Bean
+	ApplicationRunner airlineSafetyApplicationRunner() {
+		return args -> {
+			var a  = "data/airline";
+			var b = "-safety.csv" ;
+			var url = new ClassPathResource( a + b);
+			try (var in = new InputStreamReader(url.getInputStream())) {
+				var contents = FileCopyUtils.copyToString(in);
+				var lines = contents.split("\r");
+				System.out.println("there are " + lines.length + " lines");
+			}
+			;
+		};
+	}
+
+}
